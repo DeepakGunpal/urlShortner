ccd512e803c796cfe184118d8600891ba585a815
Deepak <deepakgunpal0@gmail.com>
2022-08-30 00:20:47 +0530

    redirect fixed

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 //cors
 app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type", "Access-Control-Allow-Methods": POST, GET });
+  res.header({ "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type"});
   next();
 });
 
