e0a7d2da1209efe8975e1ec742591ef38ea7f2a4
Deepak <deepakgunpal0@gmail.com>
2022-08-29 23:10:34 +0530

    final push

--- src/index.js
+++ src/index.js
@@ -16,6 +16,16 @@ app.use((req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV == "production") {
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+
 mongoose
   .connect(
     "mongodb+srv://DeepakGunpal:hdg5NWwcvf2wUDTN@deepakcluster0.hynna.mongodb.net/urlShortner",
