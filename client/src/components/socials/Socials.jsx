fb1711cd287a7b5c9627ac496dc65e79d636b6c7
Deepak <deepakgunpal0@gmail.com>
2022-08-29 23:49:23 +0530

    responsivenes added

--- client/src/components/socials/Socials.jsx
+++ client/src/components/socials/Socials.jsx
@@ -10,7 +10,7 @@ const Socials = () => {
 
             <a href='https://www.linkedin.com/in/deepak-gunpal-45186b233/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
             <a href='https://twitter.com/deepakgunpal_?s=09' target="_blank" rel="noreferrer"><FaTwitterSquare /></a>
-            <a href='https://www.deepakgunpal.com' target="_blank" rel="noreferrer"><img src={dg} /></a>
+            <a href='https://www.deepakgunpal.com' target="_blank" rel="noreferrer"><img src={dg} alt='' /></a>
             <a href='https://github.com/DeepakGunpal' target="_blank" rel="noreferrer"><FaGithubSquare /></a>
             <a href='https://www.instagram.com/gunpal.deepak?r=nametag' target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
         </div>
